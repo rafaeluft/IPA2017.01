0c6a8fe03f0298374335ddc04facafd7eb3e9329
Rafael Lima <rafaelccomp@gmail.com>
2017-10-23 15:36:23 -0300

    Aula 12 Arrays

---
+++ Aula12 - arrays/cmake-build-debug/CMakeFiles/Aula12___arrays.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aula12___arrays.dir/main.c.o"
+  "Aula12___arrays.pdb"
+  "Aula12___arrays"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Aula12___arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Aula12 - arrays/vetor1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  srand(time(NULL));
+  int V[10] = {[0] = 1, [2] = 3}, i;
+  int V2[10] = {0}, j;
+  for(i=0; i<10; i++){
+    V2[i] = rand() % 100;
+    printf("%d, ", V2[i]);
+  }
+  //Media e variancia do vetor
+  return 0;
+}
